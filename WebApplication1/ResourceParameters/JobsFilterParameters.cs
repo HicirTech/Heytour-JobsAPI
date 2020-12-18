505203bed1a8951ecf88d2bf72aef7b9d1555498
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2020-12-18 14:16:32 +1300

    fix

--- WebApplication1/ResourceParameters/JobsFilterParameters.cs
+++ WebApplication1/ResourceParameters/JobsFilterParameters.cs
@@ -7,6 +7,10 @@ namespace WebApplication1.ResourceParameters
 {
     public class JobsFilterParameters
     {
+
+        /// <summary>
+        /// enabled nullable for is active
+        /// </summary>
         public bool? IsActive { get; set; }
         public DateTime PostedOn { get; set; }
 
